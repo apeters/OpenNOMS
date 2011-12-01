80eeed653a9c3e93846bb647a9ae528e524a4d86
Alexei Peters <apeters@fargeo.com>
2011-12-01 15:28:59 -0800

    database load fixes added backup files for opennoms and alias schemas

--- apps/flighttracker/media/js/controller/OpenNoms-controllers-Query.js
+++ apps/flighttracker/media/js/controller/OpenNoms-controllers-Query.js
@@ -1,7 +1,7 @@
 ﻿Ext.define('OpenNoms.controller.Query', {
     extend: 'Ext.util.Observable',
     alias: 'controller.opennoms-controller-query',
-    
+
     constructor: function (config) {
         this.addEvents({
             "queryupdated": true
@@ -15,7 +15,60 @@
     updateQuery: function (mapPanel) {
         // here we'd get the time values for example and update the static flight track
         // for now lets just do this until we understand the time picker better
-        mapPanel.staticflightlayer.mergeNewParams({ viewparams: "" });
+        mapPanel.staticflightlayer.mergeNewParams({ viewparams: this.formatParamsForGeoserver() });
+    },
+
+
+    getFlightParams: function () {
+        var flights = Ext.getCmp('select-flights');
+        var params = {};
+        var airlines, runways, adflags;
+        var arr = [];
+
+        // get the selected airlines 
+        var airlines = flights.store.queryBy(function (rec, id) { return rec.get('group') == 'Airline' ? true : false; });
+        Ext.each(airlines.items, function (item, index, allItems) {
+            if (item.data.ischecked) {
+                arr.push(item.data.value);
+            }
+        }, this);
+
+        params.airline = arr.join(',');
+        arr = [];
+
+        // get the selected adflags 
+        var adflags = flights.store.queryBy(function (rec, id) { return rec.get('group') == 'Flight Type' ? true : false; });
+        Ext.each(adflags.items, function (item, index, allItems) {
+            if (item.data.ischecked) {
+                arr.push(item.data.value);
+            }
+        }, this);
+
+        params.adflag = arr.join(',');
+        arr = [];
+
+        // get the selected runways 
+        var runways = flights.store.queryBy(function (rec, id) { return rec.get('group').substr(0, 7) == 'Airport' ? true : false; });
+        Ext.each(runways.items, function (item, index, allItems) {
+            if (item.data.ischecked) {
+                arr.push(item.data.value);
+            }
+        }, this);
+
+        params.runway = arr.join(',');
+        arr = [];
+
+        return params;
+    },
+
+    formatParamsForGeoserver: function () {
+        var params = this.getFlightParams();
+        var ret = "";
+        for (var propertyName in params) {
+            ret += propertyName + ':' + params[propertyName] + ';';
+        }
+
+        return ret;
     }
 
 
