6c947d6a0aa69ba7f60bb4859d22270cb644f4c5
tbarger <tbarger@stanford.edu>
2012-08-23 13:21:20 -0700

    Add GPL messages
    Added General Public License disclaimers to code files, also added a
    license.txt file with an extended explanation of the disclaimers.

--- apps/flighttracker/media/js/widgets/OpenNoms-widgets-AppPanel.js
+++ apps/flighttracker/media/js/widgets/OpenNoms-widgets-AppPanel.js
@@ -1,4 +1,24 @@
-﻿Ext.define('OpenNoms.widgets.AppPanel', {
+﻿/*
+OpenNOMS (Noise and Operations Management System)
+    Copyright (C) 2012  Farallon Geographics, Inc
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	
+Contact: info@fargeo.com
+*/
+
+Ext.define('OpenNoms.widgets.AppPanel', {
     extend: 'Ext.panel.Panel',
     alias: 'widget.opennoms-widgets-apppanel',
     border: false,
