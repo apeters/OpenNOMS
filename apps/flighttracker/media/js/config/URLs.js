d0e9de9046912be14e198030454de5ba92f61c58
Alexei Peters <apeters@fargeo.com>
2012-08-29 16:34:58 -0700

    Merge

--- apps/flighttracker/media/js/config/URLs.js
+++ apps/flighttracker/media/js/config/URLs.js
@@ -1,4 +1,24 @@
-﻿Ext.namespace('OpenNoms.config');
+﻿/*
+OpenNOMS (Noise and Operations Management System)
+    Copyright (C) 2012  Farallon Geographics, Inc
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	
+Contact: info@fargeo.com
+*/
+
+Ext.namespace('OpenNoms.config');
 OpenNoms.config.URLs = {
     app: 'http://localhost/flighttracker/',
     ows: 'http://localhost:8080/geoserver/opennoms/ows',
